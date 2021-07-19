ab7ecbb85b9f05e800b0fefd5a33bc1bfc21a51f
Bishal Neupane <neupanebishal07@gmail.com>
2021-07-19 06:42:10 +0545

    HH

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { TextField, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Flare } from '@material-ui/icons';
+import React from 'react';
 import { useQueryClient } from 'react-query';
 import { useRegisterMutation } from '../generated';
 
@@ -5,7 +9,37 @@ function home() {
   const queryClient = useQueryClient();
   const { mutate } = useRegisterMutation({});
 
-  return <div>this is the awesome page</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <form style={{ width: '25%', marginTop: '20px' }}>
+        <TextField
+          required
+          label="username"
+          id="username"
+          fullWidth
+          margin="dense"
+          helperText="username must be unique"
+        />
+
+        <TextField required label="email" id="email" fullWidth margin="dense" />
+
+        <TextField
+          required
+          label="password"
+          id="password"
+          fullWidth
+          margin="dense"
+          helperText="password must be atleast 6 character long"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default home;
