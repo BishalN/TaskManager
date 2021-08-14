d97734c48b68a2a2f78ead77508564c81aa2b1d3
Bishal Neupane <neupanebishal07@gmail.com>
2021-08-14 11:15:40 +0545

    Deployment setup

---
+++ server/deploy.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+yarn build:server
+heroku container:push --app=task-manager-bishal web
+heroku container:release --app=task-manager-bishal web
+# docker build -t benawad/abb:latest .
+# docker push benawad/abb:latest
+# ssh root@167.99.11.233 "docker pull benawad/abb:latest && docker tag benawad/abb:latest dokku/abb:latest && dokku tags:deploy abb latest"
